fc3e7033cad93f464d3389064548b21013ede5a9
dskssk <dsk.ssk@gmail.com>
2017-01-09 22:18:54 +0900

    タグのコンパイル後ファイルをバージョン管理から除外

--- view/xxx.js
+++
@@ -1,3 +0,0 @@
-
-riot.tag2('foo-bars', '<div each="{item in opts.items}"><span>{item.name}</span><a href="{item.href}">{item.title}</a><span>文字数: {item.char_count} </span></div>', '', '', function(opts) {
-});
